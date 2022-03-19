198380bad2bfd5b1f3f89dda1994fb814c99f223
Pavel_Zhandin <pavelzhandin@gmail.com>
2022-03-19 20:19:49 +0300

    navigation and header finished

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
-const allSelectors = document.querySelectorAll('.main-content');
+const allSections = document.querySelector('.main-content'); //<body>
 
 function pageTransitions(){
   // Button click active class
@@ -12,6 +12,28 @@ function pageTransitions(){
       this.className +=' active-btn';
     })
   }
+
+  //Sections Active class
+  allSections.addEventListener('click', (e)=>{
+      console.log(e.target);
+      const id = e.target.dataset.id;
+      if(id){
+        //remove selected from the other btns
+        sectBtn.forEach(btn=>{
+          btn.classList.remove('active');        
+        });
+        e.target.classList.add('active');
+
+        //hide other sections
+        sections.forEach((section)=>{
+          section.classList.remove('active');
+        });
+
+        const element = document.getElementById(id);
+        element.classList.add('active')
+
+      }
+    });
 }
 
 pageTransitions();
